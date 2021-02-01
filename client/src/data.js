0ed8165d072480f7eafcd592ec6e16fd8c6e4910
gonzalo <sundbladgonzalo@gmail.com>
2021-02-01 18:44:28 -0300

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim SHirt',
+            image: './images/p1.jpg',
+            price: 150,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'high quality product'
+        }, 
+        {
+            _id: '2',
+            name: 'Adidas Slim SHirt',
+            image: './images/p2.jpg',
+            price: 120,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 50,
+            description: 'high quality product'
+        }, 
+        {
+            _id: '3',
+            name: 'NIDEA Slim SHirt',
+            image: './images/p3.jpg',
+            price: 100,
+            brand: 'qcyo',
+            rating: 4.5,
+            numReviews: 40,
+            description: 'high quality product'
+        }, 
+        {
+            _id: '4',
+            name: 'Boca Slim SHirt',
+            image: './images/p4.jpg',
+            price: 220,
+            brand: 'Boca',
+            rating: 4.5,
+            numReviews: 850,
+            description: 'high quality product'
+        }, 
+        {
+            _id: '5',
+            name: 'Lacademia Slim SHirt',
+            image: './images/p5.jpg',
+            price: 155,
+            brand: 'Racing',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        }, 
+        {
+            _id: 6,
+            name: 'Diegote Slim SHirt',
+            image: './images/p6.jpg',
+            price: 1200,
+            brand: 'd10s',
+            rating: 4.5,
+            numReviews: 1000,
+            description: 'high quality product'
+        }, 
+    ]
+}
+
+export default data;
