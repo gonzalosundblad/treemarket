248fff3c91f96be49cf4f2af10a48286f28cdefc
gonzalo <sundbladgonzalo@gmail.com>
2021-02-02 11:17:29 -0300

    probar un temita

--- client/src/screens/homeScreen.js
+++ client/src/screens/homeScreen.js
@@ -8,7 +8,7 @@ export default function HomeScreen () {
         <div className="row center">
           {
             data.products.map(product => (
-              <Product key={product._id} product={product}></Product>
+              <Product key={product._id} product={product}></Product> 
             ))
           }
         </div>
